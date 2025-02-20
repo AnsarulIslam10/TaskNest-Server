925c5cc40bed8a13f0b45cf5b1a5c6fec609e0ef
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-02-20 14:06:00 +0600

    category update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const cors = require("cors")
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ggzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -17,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -25,24 +25,33 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
     const taskCollection = client.db("TaskNestDB").collection("tasks");
 
-    app.post("/add-task", async (req, res)=>{
-        const newTask = req.body;
-        const result = await taskCollection.insertOne(newTask)
-        res.send(result)
-    })
+    app.post("/add-task", async (req, res) => {
+      const newTask = req.body;
+      const result = await taskCollection.insertOne(newTask);
+      res.send(result);
+    });
 
-    app.get('/tasks', async(req, res)=>{
-        const result = await taskCollection.find().toArray()
-        res.send(result)
-    })
+    app.get("/tasks", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
 
-    
+    app.put("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const { category } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = { $set: { category } };
+      const result = await taskCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -50,17 +59,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
